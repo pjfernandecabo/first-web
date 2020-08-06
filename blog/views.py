215f569631e90adc361406f4b59c57c0e65b09de
Pedro Fernandez <pedro.fdc@gmail.com>
2020-08-06 13:02:10 -0500

    anadiendo plantilla base mas enlace a posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+
 
 from .models import Post
 
@@ -11,3 +13,6 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
